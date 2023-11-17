2c76fde411467279087cb68fb5061819e48e79e9
Stckrz <rin449@gmail.com>
2023-11-17 16:00:00 -0600

    regular auto-commit

---
+++ src/library/rarityStyles.js
@@ -0,0 +1,16 @@
+
+export const commonStyle = {
+	color: "#1eff00"
+}
+
+export const uncommonStyle = {
+	color: "#0070dd"
+}
+
+export const rareStyle = {
+	color: "#a335ee"
+}
+
+export const leggoStyle = {
+	color: "#ff8000"
+}
