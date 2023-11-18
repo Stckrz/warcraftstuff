d2e48657cd121c6bdd1ff332f04e49e37a82cff4
Stckrz <rin449@gmail.com>
2023-11-17 18:03:43 -0600

    fixed a bug with color styles

---
+++ src/components/mythicrun/vaultstats/vaultbox/vaultbox.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import './vaultbox.css';
+import * as rarityStyles from 'library/rarityStyles.js'
+
+
+export function VaultBox(props) {
+	const { boxnumber, keylevel, ilvlreward } = props;
+	const [rarityColor, setRarityColor] = useState({color: "white"});
+
+
+	function rarityColorHandler() {
+		keylevel < 10 ? setRarityColor(rarityStyles.commonStyle)
+			: keylevel < 15 ? setRarityColor(rarityStyles.uncommonStyle)
+				: keylevel < 20 ? setRarityColor(rarityStyles.rareStyle)
+					: setRarityColor(rarityStyles.leggoStyle)
+	
+	}
+
+	useEffect(() => {
+		rarityColorHandler()
+
+	}, [keylevel])
+
+
+
+
+	return (
+		<>
+			<div className="reward-wrapper" style={
+				{ border: `1px solid ${rarityColor.color}`, boxShadow: `0px 0px 5px 3px ${rarityColor.color}` }
+			}>
+				reward {boxnumber}:
+					<div className="key-level"
+						style={
+							{ color: rarityColor.color }
+						}>{keylevel}</div>
+
+				<div className="ilvl-reward"
+					style={
+						{ color: rarityColor.color }
+					}>{ilvlreward}</div>
+			</div>
+
+		</>
+
+
+	)
+
+
+
+
+
+
+}
--- src/components/mythicrun/vaultstats/vaultstats.js
+++ src/components/mythicrun/vaultstats/vaultstats.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import './vaultstats.css';
 import { vaultrewardsarray } from './vaultrewards';
+import { VaultBox } from './vaultbox/vaultbox';
 
 export function VaultStats(props) {
 	const { rundata } = props;
 	const [weeklyKeyNumbers, setWeeklyKeyNumbers] = useState([]);
 
+	function findIlvlReward(i) {
+		return vaultrewardsarray.find(item => item.level === weeklyKeyNumbers[i]).reward
+	}
 
 	function getWeeklyKeyLevels(arr) {
 		let levelsArr = []
@@ -25,25 +29,30 @@ export function VaultStats(props) {
 	return (
 		<>
 			<div className="vaultwrapper">
-			{weeklyKeyNumbers.length >= 1 ?
-				<div className="reward-wrapper">
-						reward 1: <div>{weeklyKeyNumbers[0]}</div> 
-					<div>{vaultrewardsarray.find(item => item.level === weeklyKeyNumbers[0]).reward}</div>
-					</div>:<div>reward 1: --</div>}
+				{weeklyKeyNumbers.length >= 1 ?
+					<VaultBox 
+						boxnumber={1} 
+						keylevel={weeklyKeyNumbers[0]} 
+						ilvlreward={findIlvlReward(0)} 
+					/>
+					: <div className="reward-wrapper">reward 1: <div>--</div></div>}
 
-			{weeklyKeyNumbers.length >= 3 ?
-				<div className="reward-wrapper">
-						reward 2: <div>{weeklyKeyNumbers[2]}</div> 
-					<div>{vaultrewardsarray.find(item => item.level === weeklyKeyNumbers[2]).reward}</div>
+				{weeklyKeyNumbers.length >= 3 ?
+					<VaultBox 
+						boxnumber={2} 
+						keylevel={weeklyKeyNumbers[2]} 
+						ilvlreward={findIlvlReward(2)} 
+					/>
+					: <div className="reward-wrapper">reward 2: <div>--</div></div>}
 
-					</div>:<div>reward 2: --</div>}
-			
-			{weeklyKeyNumbers.length >= 8 ? 
-				<div className="reward-wrapper">
-						reward 3: <div>{weeklyKeyNumbers[7]}</div>
-					<div>{vaultrewardsarray.find(item => item.level === weeklyKeyNumbers[7]).reward}</div>
-					</div>:<div>reward 3: --</div>}
-				</div>
+				{weeklyKeyNumbers.length >= 8 ?
+					<VaultBox 
+						boxnumber={3} 
+						keylevel={weeklyKeyNumbers[7]} 
+						ilvlreward={findIlvlReward(7)} 
+					/>
+					: <div className="reward-wrapper">reward 3: <div>--</div></div>}
+			</div>
 		</>
 
 
