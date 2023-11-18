84c2277727cdb892300875d724e945dfb7afe1e9
Stckrz <rin449@gmail.com>
2023-11-18 11:11:13 -0600

    regular auto-commit

--- src/library/rarityStyles.js
+++ src/library/rarityStyles.js
@@ -14,3 +14,14 @@ export const rareStyle = {
 export const leggoStyle = {
 	color: "#ff8000"
 }
+
+//returns a hexcode color value based on key level
+export function keyRarityColorHandler(keylevel){
+	let	color = "";
+	keylevel < 10 ? color = commonStyle.color
+	: keylevel < 15? color = uncommonStyle.color
+	: keylevel < 20 ? color = rareStyle.color
+	: color = leggoStyle.color;
+	return color;
+}
+
